8ef95a09fe7f50299a5b70c77e4f47ba4ed56099
Platireum <platireum@gmail.com>
2025-05-19 19:47:16 +0300

    Update Main Hen Token Contract (ERC-20).sol

--- contracts/Main Hen Token Contract (ERC-20).sol
+++ contracts/Main Hen Token Contract (ERC-20).sol
@@ -1,3 +1,10 @@
+// * Important Notice: Terms of Use for Platireum Currency: By receiving this Platireum currency, you irrevocably acknowledge and solemnly pledge your full adherence to the subsequent terms and conditions:
+// * 1- Platireum must not be used for fraud or deception.
+// * 2- Platireum must not be used for lending or borrowing with interest (usury).
+// * 3- Platireum must not be used to buy or sell intoxicants, narcotics, or anything that impairs judgment.
+// * 4- Platireum must not be used for criminal activities and money laundering.
+// * 5- Platireum must not be used for gambling.
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
