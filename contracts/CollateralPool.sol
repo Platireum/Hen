6e14cf5335fac3715d6fba1a6ff5b1d114f88172
Platireum <platireum@gmail.com>
2025-09-02 15:29:20 +0300

    Update and rename CollateralPool_02.sol to CollateralPool.sol

--- contracts/CollateralPool.sol
+++ contracts/CollateralPool.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./Platireum.sol";
+import "./Platireum_HEN.sol";
 
 /**
  * @title CollateralPool
